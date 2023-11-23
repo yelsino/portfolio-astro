5c629b8ae3a1012067da09d5bba81c363c0ea72d
yelsiño <yelsino321@gmai.com>
2023-11-23 02:28:52 -0500

    cambie a hybrid renderization

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()]
-  // integrations: [ svelte()]
   ,
-  output: "server",
+  output: "hybrid",
   adapter: vercel()
 });
